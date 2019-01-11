df0c396f01be07c2a0debc4674ff2f979b18894c
itachi1706 <kennethsohyq@gmail.com>
2019-01-11 22:23:31 +0800

    Releasing 1.7 update
    - Comes with Custom Skin support

--- src/main/java/com/itachi1706/Forge/CheesecakeCapes/CheesecakeCapes.java
+++ src/main/java/com/itachi1706/Forge/CheesecakeCapes/CheesecakeCapes.java
@@ -14,7 +14,7 @@ public class CheesecakeCapes {
 
     //Essential variables for Mod info
     public static final String MOD_ID = "cheesecakecapes";
-    public static final String VERSION = "1.12.2-1.6";
+    public static final String VERSION = "1.12.2-1.7";
     public static final String MOD_NAME = "Cheesecake Capes Mod";
 
     @SidedProxy(clientSide = "com.itachi1706.Forge.CheesecakeCapes.CheesecakeClientProxy")
