b3dbd24165f4c0159eda9d7c2825c312e2ffb943
itachi1706 <kennethsohyq@gmail.com>
2019-01-11 19:09:05 +0800

    Initial Migration and update of code to 12 September 2016 codebase

--- src/main/java/com/jadarstudios/developercapes/RenderEventHandler.java
+++ src/main/java/com/jadarstudios/developercapes/RenderEventHandler.java
@@ -10,7 +10,7 @@ import com.jadarstudios.developercapes.cape.ICape;
 import com.jadarstudios.developercapes.user.User;
 import com.jadarstudios.developercapes.user.UserManager;
 import net.minecraft.client.entity.AbstractClientPlayer;
-import net.minecraftforge.client.event.RenderLivingEvent;
+import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 /**
@@ -21,21 +21,19 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class RenderEventHandler {
 
     @SubscribeEvent
-    public void renderPlayer(RenderLivingEvent.Specials.Post event) {
-        if (event.getEntity() instanceof AbstractClientPlayer) {
-            AbstractClientPlayer player = (AbstractClientPlayer) event.getEntity();
+    public void renderPlayer(RenderPlayerEvent.Specials.Pre event) {
+        AbstractClientPlayer player = (AbstractClientPlayer) event.entityPlayer;
 
-            UserManager manager = UserManager.getInstance();
-            User user = manager.getUser(player.getName());
-            if (user == null) return;
+        UserManager manager = UserManager.getInstance();
+        User user = manager.getUser(player.getUniqueID().toString());
+        if (user == null) return;
 
-            ICape cape = user.capes.get(0);
-            if (cape == null) return;
+        ICape cape = user.capes.get(0);
+        if (cape == null) return;
 
-            boolean flag = cape.isTextureLoaded(player);
-            if (!flag) {
-                cape.loadTexture(player);
-            }
+        boolean flag = cape.isTextureLoaded(player);
+        if (!flag) {
+            cape.loadTexture(player);
         }
     }
 }
